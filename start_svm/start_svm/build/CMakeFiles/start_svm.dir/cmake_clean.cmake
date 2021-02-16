ac6e15a0eedbb0abee8eb5097b31fb01dc2fcc9f
Colin <colin.baumgard@gmail.com>
2021-02-16 11:49:27 +0100

    ras

---
+++ start_svm/start_svm/build/CMakeFiles/start_svm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/start_svm.dir/main.cpp.o"
+  "start_svm"
+  "start_svm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/start_svm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
